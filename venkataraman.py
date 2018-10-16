c0d4e0eaa0fef07bb99ba56c8e87840cc02606c2
Catherine Zeng <yczeng@mit.edu>
2018-10-16 17:50:42 -0400

    this is almost working. Need to figure out how to record new segmentations

--- venkataraman.py
+++ venkataraman.py
@@ -2,10 +2,10 @@
 
 import math
 
-# lexicon is a dictionary of tuples where tuples are phonemes representing words and the values represent frequency
+# lexicon is a dictionary or words and their respective frequencies
 lexicon = {}
 
-# this is probably just strings as keys and frequencies as values
+# phonemes is a dictionary of phonemes and their respective frequencies.
 phonemes = {" ": 1, "#": 1, "%": 1, "&": 1, "(": 1, ")": 1, "*": 1, "3": 1, "6": 1, "7": 1, "9": 1, "A": 1, "D": 1, "E": 1, "G": 1, "I": 1, "L": 1, "M": 1, "N": 1, "O": 1, "Q": 1, "R": 1, "S": 1, "T": 1, "U": 1, "W": 1, "Z": 1, "a": 1, "b": 1, "c": 1, "d": 1, "e": 1, "f": 1, "g": 1, "h": 1, "i": 1, "k": 1, "l": 1, "m": 1, "n": 1, "o": 1, "p": 1, "r": 1, "s": 1, "t": 1, "u": 1, "v": 1, "w": 1, "y": 1, "z": 1, "~": 1}
 
 def evalUtterance(utterance):
@@ -35,33 +35,11 @@ def evalUtterance(utterance):
 	# not sure what the point of this is. It seems to just set i to 0?
 	while i > 0:
 		i = prevBoundary[i]
-		print(i)
+		print("i is", i)
 
-	insertWordBoundary(utterance, prevBoundary[i])
+	newWord = insertWordBoundary(utterance, prevBoundary[i])
 	print(utterance, bestCost[n-1])
-	return bestCost[n-1]
-
-
-# def evalUtterance(utterance):
-# 	n = len(utterance)
-
-# 	best_segment = n
-# 	bestCost = evalWord(utterance[0:n])
-# 	bestSegpoint = 0
-
-# 	for i in range(1, n):
-# 		subUtterance = utterance[0:i]
-# 		word = utterance[i::]
-# 		cost = evalUtterance(subUtterance) + evalWord(word)
-# 		if cost < bestCost:
-# 			bestCost = cost
-# 			bestSegpoint = i
-
-# 	# print("bestSegpoint is at", bestSegpoint)
-# 	# insertWordBoundary(utterance, bestSegpoint)
-
-# 	print(utterance, bestCost)
-# 	return bestCost
+	return bestCost[n-1], newWord
 
 def insertWordBoundary(utterance, bestSegpoint):
 	if bestSegpoint == -1:
@@ -77,6 +55,8 @@ def insertWordBoundary(utterance, bestSegpoint):
 	for phoneme in newWord:
 		phonemes[phoneme] += 1
 
+	print("new word is", newWord)
+	return newWord
 
 def evalWord(word):
 	'''
@@ -117,14 +97,14 @@ if __name__ == "__main__":
 		for count, line in enumerate(text):
 			processedLine = line.replace('\n', '').replace(" ", "")
 
-			evalUtterance(processedLine)
+			newWord = evalUtterance(processedLine)[1]
 			print(phonemes)
 			print("length of phonemes", len(phonemes))
 			print(lexicon)
 			print("length of lexicon", len(lexicon))
 
-			# with open('dump/progress1.txt','a') as progress:
-			# 	progress.write("We are at line: " + str(count) + "\n")
+			# with open('dump/results1.txt','a') as progress:
+			# 	progress.write(newWord + "\n")
 
 	with open('data/result1.txt','a') as result:
 		result.write(str(lexicon))
