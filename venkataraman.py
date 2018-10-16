41b4d11256af16ca1e8c6566b48469b729caced3
Catherine Zeng <yczeng@mit.edu>
2018-10-15 20:58:58 -0400

    not completely working but added iterative

--- venkataraman.py
+++ venkataraman.py
@@ -8,44 +8,78 @@ lexicon = {}
 # this is probably just strings as keys and frequencies as values
 phonemes = {'I': 1,'E': 1,'&': 1,'A': 1,'a': 1,'O': 1,'U': 1,'6': 1,'i': 1,'e': 1,'9': 1,'Q': 1,'u': 1,'o': 1,'7': 1,'3': 1,'R': 1,'#': 1,'%': 1,'*': 1,'(': 1,')': 1,'p': 1,'b': 1,'m': 1,'t': 1,'d': 1,'n': 1,'k': 1,'g': 1,'N': 1,'f': 1,'v': 1,'T': 1,'D': 1,'s': 1,'z': 1,'S': 1,'Z': 1,'h': 1,'c': 1,'G': 1,'l': 1,'r': 1,'L': 1,'~': 1,'M': 1,'y': 1,'w': 1,'W': 1}
 
-def evalUtterance(utterance):
+def evalUtteranceIterative(utterance):
 	n = len(utterance)
+	bestCost = [0] * n
+	prevBoundary = [0] * n
 
-	best_segment = n
-	bestCost = evalWord(utterance[0:n])
-	bestSegpoint = 0
+	#finally... now we have a base case
+	if n == 0:
+		return 0;
 
-	for i in range(1, n):
-		subUtterance = utterance[0:i]
-		word = utterance[i::]
-		cost = evalUtterance(subUtterance) + evalWord(word)
-		print("cost is", cost)
-		print(subUtterance, word)
-		if cost < bestCost:
-			bestCost = cost
-			bestSegpoint = i
+	for i in range(n):
+		# need to add prev word to eval word
+		bestCost[i] = evalWord(u[0:i])
+		prevBoundary[i] = -1
 
-	insertWordBoundary(utterance, bestSegpoint)
-	return bestCost
+		for j in range(i):
+			prevWord = u[prevBoundary[j] + 1:i]
+			word = u[j+1:i]
+			cost = bestCost[j] + evalWord(prevWord, word)
+
+			if cost < bestCost[i]:
+				bestCost[i] = cost
+				prevBoundary[i] = j
+
+	i = n - 1
+	while i > 0:
+		insertWordBoundary(u, prevBoundary[i])
+		i = prevBoundary[i]
+	return bestCost[n]
+
+
+# def evalUtterance(utterance):
+# 	n = len(utterance)
+
+# 	best_segment = n
+# 	bestCost = evalWord(utterance[0:n])
+# 	bestSegpoint = 0
+
+# 	for i in range(1, n):
+# 		subUtterance = utterance[0:i]
+# 		word = utterance[i::]
+# 		print("SPLIT BY UTTERANCE AND WORD", subUtterance, word)
+# 		cost = evalUtterance(subUtterance) + evalWord(word)
+# 		print("cost is", cost)
+# 		if cost < bestCost:
+# 			bestCost = cost
+# 			bestSegpoint = i
+
+# 	print("bestSegpoint is at", bestSegpoint)
+# 	insertWordBoundary(utterance, bestSegpoint)
+
+# 	print(phonemes)
+# 	print(lexicon)
+# 	return bestCost
 
 def insertWordBoundary(utterance, bestSegpoint):
 	# for inserting into word boundary
-	newWord = utterance[bestSegpoint::]
-	if newWord in lexicon:
-		lexicon[newWord] += 1
-	else:
-		lexicon[newWord] = 1
+
+
+	# newWord = utterance[bestSegpoint::]
+	# if newWord in lexicon:
+	# 	lexicon[newWord] += 1
+	# else:
+	# 	lexicon[newWord] = 1
 
 	for phoneme in newWord:
 		if phoneme in phonemes:
 			phonemes[phoneme] += 1
 		else:
 			phonemes[phonemes] = 0
-	print(phonemes)
-	print(lexicon)
 
 
-def evalWord(word):
+def evalWord(prevWord, word):
 	'''
 	Calculates a log score for word
 
